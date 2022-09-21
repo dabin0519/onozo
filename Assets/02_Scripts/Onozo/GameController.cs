f918c130b7a26df492c21e26a64f52d2c2d14690
dksgudwn <dksgudwn620@gmail.com>
2022-09-21 17:54:21 +0900

    완성

--- Assets/02_Scripts/Onozo/GameController.cs
+++ Assets/02_Scripts/Onozo/GameController.cs
@@ -15,17 +15,24 @@ public class GameController : MonoBehaviour
     [SerializeField] GameObject line3;
     [SerializeField] GameObject line4;
     [SerializeField] GameObject line5;
+    bool SetActiveOn;
     bool a = false, b = false, c = false, d = false, e = false, correct = false;
     bool lightA = false, lightB = false, lightC = false, lightD = false, lightE = false, lightF = false;
     [SerializeField] GameObject wow;
+
+    public GameObject[] buttonList;
+    public GameObject[] lineList;
+
     private void Start()
     {
+        
         field.text = "";
         text.text = "비밀번호를 입력하세요";
         wow.SetActive(false);
     }
     private void Update()
     {
+        
         correctA();
         if(field.text == pw){
             text.text = "열렸습니다.";
@@ -50,73 +57,88 @@ public class GameController : MonoBehaviour
         else
         {
             line1.SetActive(true);
-            a = true;
             lightA = false;
         }
     }
-    public void ColorChange2(){
-        if(!lightB)
-        {
-            line2.SetActive(false);
-            b = false;
-            lightB = true;
-        }
-        else
-        {
-            line2.SetActive(true);
-            b = true;
-            lightB = false;
-        }
-    }
-    public void ColorChange3(){
-        if(!lightC)
-        {
-            line3.SetActive(false);
-            c = false;
-            lightC = true;
-        }
-        else
-        {
-            line3.SetActive(true);
-            c = true;
-            lightC = false;
-        }
-    }
-    public void ColorChange4(){
-        if(!lightD)
-        {
-            line4.SetActive(false);
-            d = false;
-            lightD = true;
-        }
-        else
-        {
-            line4.SetActive(true);
-            d = true;
-            lightD = false;
-        }
-    }
-    public void ColorChange5(){
-        if(!lightE)
-        {
-            line5.SetActive(false);
-            e = false;
-            lightE = true;
-        }
-        else
-        {
-            line5.SetActive(true);
-            e = true;
-            lightE = false;
-        }
-    }
-
     public void correctA(){
         if(a && b && c && d && e){
             correct = true;
         }
         if(correct == true){
             wow.SetActive(true);
+        }
+        
+    }
+    public void Button2(int i){
+        if(i == 0){
+            if(!lightA){
+                lineList[i].SetActive(false);
+                a = false;
+                lightA = true;
+            }
+            else{
+                lineList[i].SetActive(true);
+                a = true;
+                lightA = false;
+            }
+        }
+        else if(i == 1){
+            if(lightB){
+                b = true;
+                lightB = false;
+                lineList[i].SetActive(false);
+            }
+            else{
+                b = false;
+                lightB = true;
+                lineList[i].SetActive(true);
+            }
+        }
+        else if(i == 3){
+            if(lightC){
+                c = true;
+                lightC = false;
+                lineList[i].SetActive(false);
+            }
+            else{
+                c = false;
+                lightC = true;
+                lineList[i].SetActive(true);
+            }
+        }
+        else if(i == 6){
+            if(lightD){
+                d = true;
+                lightD = false;
+                lineList[i].SetActive(false);
+            }
+            else{
+                d = false;
+                lightD = true;
+                lineList[i].SetActive(true);
+            }
+        }
+        else if(i == 14){
+            if(lightE){
+                e = true;
+                lightE = false;
+                lineList[i].SetActive(false);
+            }
+            else{
+                e = false;
+                lightE = true;
+                lineList[i].SetActive(true);
+            }
+        }
+        else{
+            if(SetActiveOn){
+                lineList[i].SetActive(false);
+                SetActiveOn = false;
+            }
+            else {
+                lineList[i].SetActive(true);
+                SetActiveOn = true;
+            }
         }
     }
 }
